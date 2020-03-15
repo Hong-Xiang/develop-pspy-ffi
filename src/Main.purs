20cc99281e3feb87d998313211ccd12ab26e2a8f
Xiang <hx.hongxiang@gmail.com>
2020-03-15 16:55:10 +0800

    init

---
+++ src/Main.purs
@@ -0,0 +1,23 @@
+module Main where
+
+import Prelude
+import Data.Maybe (Maybe(Nothing, Just))
+import Data.Tuple (Tuple(Tuple))
+import Data.Unfoldable (unfoldr)
+
+import Effect (Effect)
+import Effect.Console (log)
+
+gen :: Int -> Maybe (Tuple Int Int)
+gen 0 = Nothing
+gen n = Just (Tuple n (n - 1))
+
+values :: Array Int
+values = unfoldr gen 10
+
+main :: Effect Unit
+main = do
+  log "🍝"
+  log "Test Hello World"
+  log $ show [1,2,3]
+  log $ show values
